6e0e8a9ba03ec82912e11d7dfc0bc17bae38935d
Bret Stateham <Bret.Stateham@microsoft.com>
2013-11-10 19:43:25 -0800

    Partial work on Data Access

---
+++ 04-DataAccessTechnologies/Demos/Prep/AdoNetDemo/AdoNetDemo.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo.Console
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string constr = "Server=tcp:djdrwgqf0p.database.windows.net,1433;Database=Positions;User ID=sqladmin@djdrwgqf0p;Password=Pa$$w0rd;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+      using(SqlConnection connection = new SqlConnection(constr))
+      {
+        System.Console.WriteLine(connection.State.ToString());
+        using (SqlCommand cmd = new SqlCommand("SELECT TOP 10 PositionID, ReportedAt, Latitude, Longitude FROM dbo.Positions;",connection))
+        {
+          connection.Open();
+          using (SqlDataReader rdr = cmd.ExecuteReader())
+          {
+            while(rdr.Read())
+            {
+              string position = string.Format("{0},{1},{2},{3}",rdr.GetInt32(0),rdr.GetDateTime(1),rdr.GetFloat(2),rdr.GetFloat(3));
+              System.Console.WriteLine(position);
+            }
+          }
+        }
+      }
+      System.Console.WriteLine("Press <ENTER> to continue...");
+      System.Console.ReadLine();
+    }
+  }
+}
---
+++ 04-DataAccessTechnologies/Demos/Prep/AdoNetDemo/AdoNetDemo/default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AdoNetDemo
+{
+  public partial class _default : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      if(!IsPostBack)
+        PopulateGrid();
+    }
+
+    private void PopulateGrid()
+    {
+      string constr = "Server=tcp:djdrwgqf0p.database.windows.net,1433;Database=Positions;User ID=sqladmin@djdrwgqf0p;Password=Pa$$w0rd;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+      using(SqlConnection connection = new SqlConnection(constr))
+      {
+        System.Console.WriteLine(connection.State.ToString());
+        using (SqlCommand cmd = new SqlCommand("SELECT TOP 10 PositionID, ReportedAt, Latitude, Longitude FROM dbo.Positions;",connection))
+        {
+          connection.Open();
+          using (SqlDataReader rdr = cmd.ExecuteReader())
+          {
+            PositionsGrid.DataSource = rdr;
+            PositionsGrid.DataBind();
+          }
+        }
+      }
+    }
+  }
+}
