8e14a55af64db952559bfa4a825bfc3c0519a68a
Bret Stateham <Bret.Stateham@microsoft.com>
2013-11-11 13:25:37 -0800

    Continued 04 Work

--- 04-DataAccessTechnologies/Demos/Prep/AdoNetDemo/AdoNetDemo.Console/Program.cs
+++ 04-DataAccessTechnologies/Demos/Prep/AdoNetDemo/AdoNetDemo.Console/Program.cs
@@ -12,17 +12,19 @@ namespace AdoNetDemo.Console
     static void Main(string[] args)
     {
       string constr = "Server=tcp:djdrwgqf0p.database.windows.net,1433;Database=Positions;User ID=sqladmin@djdrwgqf0p;Password=Pa$$w0rd;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
-      using(SqlConnection connection = new SqlConnection(constr))
+      using (SqlConnection connection = new SqlConnection(constr))
       {
         System.Console.WriteLine(connection.State.ToString());
-        using (SqlCommand cmd = new SqlCommand("SELECT TOP 10 PositionID, ReportedAt, Latitude, Longitude FROM dbo.Positions;",connection))
+        string query = "SELECT TOP 10 PositionID, ReportedAt, Latitude, Longitude FROM dbo.Positions;";
+        using (SqlCommand cmd = new SqlCommand(query, connection))
         {
           connection.Open();
           using (SqlDataReader rdr = cmd.ExecuteReader())
           {
-            while(rdr.Read())
+            while (rdr.Read())
             {
-              string position = string.Format("{0},{1},{2},{3}",rdr.GetInt32(0),rdr.GetDateTime(1),rdr.GetFloat(2),rdr.GetFloat(3));
+              string position = 
+                string.Format("{0},{1},{2},{3}", rdr.GetInt32(0), rdr.GetDateTime(1), rdr.GetFloat(2), rdr.GetFloat(3));
               System.Console.WriteLine(position);
             }
           }
