1d90eae6fbccf860a8c140e197df1ccfb1118b94
Bret Stateham <Bret.Stateham@microsoft.com>
2013-11-12 08:15:56 -0800

    Almost done with EF Deck

---
+++ 04-DataAccessTechnologies/Database/Return Positions as XML.sql
@@ -0,0 +1,39 @@
+﻿-- =============================================================================
+-- Return all positions using FOR XML PATH to create mixed elements
+-- =============================================================================
+
+SELECT
+   Position.PositionID AS [@PositionID]
+  ,Position.CruiseID AS [@CruiseID]
+  ,Position.ReportedAt
+  ,Place.AdminName AS Region
+  ,Place.CountryName AS Country
+  ,CAST(Position.Latitude AS Decimal(11,8)) AS Latitude
+  ,CAST(Position.Longitude AS Decimal(11,8)) AS Longitude
+FROM dbo.Positions as Position
+JOIN dbo.Places AS Place
+ON Position.PlaceID = Place.PlaceID
+ORDER BY Position.PositionID
+FOR XML PATH('Position'), ROOT('Positions')
+
+
+-- =============================================================================
+-- Return a sampling of positions using FOR XML PATH to create mixed elements
+-- =============================================================================
+
+SELECT TOP 100
+   Position.PositionID AS [@PositionID]
+  ,Position.CruiseID AS [@CruiseID]
+  ,Position.ReportedAt
+  ,Place.AdminName AS Region
+  ,Place.CountryName AS Country
+  ,CAST(Position.Latitude AS Decimal(11,8)) AS Latitude
+  ,CAST(Position.Longitude AS Decimal(11,8)) AS Longitude
+FROM dbo.Positions as Position
+JOIN dbo.Places AS Place
+ON Position.PlaceID = Place.PlaceID
+WHERE Position.CruiseID IN (1,2) 
+  AND Position.PositionID % 10 = 0
+ORDER BY Position.PositionID
+FOR XML PATH('Position'), ROOT('Positions')
+
---
+++ 05-EntityFramework/Demo/Prep/EfDemos.DesignFirst/EfDemos.DesignFirst/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfDemos.DesignFirst
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      //UseRelationships();
+      GetDistinct();
+
+    }
+
+    private static void GetDistinct()
+    {
+      PositionsEntities ctx = new PositionsEntities();
+
+      var cruiseNames = (from p in ctx.Positions
+                         select p.Cruise.Name).Distinct();
+
+      foreach (var name in cruiseNames)
+        Console.WriteLine("{0}", name);
+
+    }
+
+    private static void UseRelationships()
+    {
+      PositionsEntities ctx = new PositionsEntities();
+
+      var cruise1 = from p in ctx.Positions
+                    where p.CruiseID == 3
+                    select new
+                    {
+                      p.PositionID,
+                      p.Latitude,
+                      p.Longitude,
+                      p.ReportedAt,
+                      p.CruiseID,
+                      p.Cruise.Name
+                    };
+
+      foreach (var p in cruise1)
+        Console.WriteLine("{0}:{1},{2} - {3},{4}", p.PositionID, p.Latitude, p.Longitude, p.ReportedAt, p.Name);
+    }
+  }
+}
